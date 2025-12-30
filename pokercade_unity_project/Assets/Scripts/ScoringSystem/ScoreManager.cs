f3e21e39db68339efda3ad181cec958fe8e069de
Leo Caldosa <caldosaleo@gmail.com>
2025-12-30 16:54:36 +0800

    remade flow for detection flow for check_full_house_3_of_a_kind_pairs and make functions return default

--- pokercade_unity_project/Assets/Scripts/ScoringSystem/ScoreManager.cs
+++ pokercade_unity_project/Assets/Scripts/ScoringSystem/ScoreManager.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEditor.PackageManager;
+using Unity.Jobs;
+using UnityEngine.Rendering.Universal;
 
 public class ScoreManager : MonoBehaviour
 {
@@ -25,15 +27,15 @@ public class ScoreManager : MonoBehaviour
             played_cards.Add(card_transform.gameObject);
         }
 
-        BaseScoreData base_score = null;
+        BaseScoreData base_score = new BaseScoreData();
         base_score = check_straight_and_or_flush(played_cards);
-        if (base_score == null) { base_score = check_four_of_a_kind(played_cards); }
-        if (base_score == null) { base_score = check_full_house_3_of_a_kind_pairs(played_cards); }
-        if (base_score == null) { base_score = get_high_card(played_cards); }
+        if (base_score == default) { base_score = check_four_of_a_kind(played_cards); }
+        if (base_score == default) { base_score = check_full_house_3_of_a_kind_pairs(played_cards); }
+        if (base_score == default) { base_score = get_high_card(played_cards); }
 
         foreach (GameObject scored_card in base_score.scored_cards)
         {
-            base_score.score_value = base_score.score_value + scored_card.GetComponent<CardData>().Card.value;
+            base_score.score_value = base_score.score_value + scored_card.GetComponent<CardData>().get_value();
         }
         score = base_score.score_value * base_score.score_mult;
 
@@ -49,78 +51,75 @@ public class ScoreManager : MonoBehaviour
         List<GameObject> straight = check_straight(played_cards);
         List<GameObject> flush = check_flush(played_cards);
 
-        if (straight != null && flush != null) 
+        if (straight != default && flush != default) 
         {
             // Straight flush is played
             // Adjust value of base_score for straight flush
             return base_score;
         }
-        else if (straight != null)
+        else if (straight != default)
         {
             // Straight is played
             // Adjust value of base_score for straight
             return base_score;
         }
-        else if (flush != null)
+        else if (flush != default)
         {
             // Flush is played
             // Adjust value of base_score for flush
             return base_score;
         }
         // else both straight and flush is null, return null
-        return null;
+        return default;
     }
 
     public static List<GameObject> check_straight(List<GameObject> played_cards)
     {
         List<GameObject> scored_cards = new List<GameObject>();
-        return scored_cards;
+        return default;
     }
 
     public static List<GameObject> check_flush(List<GameObject> played_cards)
     {
         List<GameObject> scored_cards = new List<GameObject>();
-        return scored_cards;
+        return default;
     }
 
     public static BaseScoreData check_four_of_a_kind(List<GameObject> played_cards)
     {
         // check if four of a kind is played
         BaseScoreData base_score = new BaseScoreData();
-        return base_score;
+        return default;
     }
 
     public static BaseScoreData check_full_house_3_of_a_kind_pairs(List<GameObject> played_cards)
     {
         BaseScoreData base_score = new BaseScoreData();
         List<GameObject> pairs = check_for_pairs(played_cards);
-        if (pairs == null)
+        List<GameObject> three_of_a_kind = check_three_of_a_kind(played_cards);
+        if (pairs != default && pairs.Count == 2)
         {
-            return null;
-        }
-        else if (pairs.Count == 4)
-        {
-            // adjust base_score for two pair
-            return base_score;
-        }
-        else // if pairs.Count == 2
-        {
-            List<GameObject> three_of_a_kind = check_three_of_a_kind(played_cards);
-            if (three_of_a_kind != null && pairs.Count == 2)
+            if (three_of_a_kind != default)
             {
                 // adjust base_score for full house
-                return base_score;
+                return default;
             }
-            else if (three_of_a_kind != null)
-            {
-                // adjust base_score for three of a kind
-                return base_score;
-            }
-            else
-            {
-                // adjust base_score for pair
-                return base_score;
-            }
+            // adjust base_score for pair
+            return default;
+        }
+        else if (pairs != default && pairs.Count == 4) 
+        {
+            // adjust base_score for two pair
+            return default;
+        }
+        else if (three_of_a_kind != default)
+        {
+            // adjust base_score for three of a kind
+            return default;
+        }
+        else
+        {
+            return default;
         }
     }
 
@@ -128,34 +127,34 @@ public class ScoreManager : MonoBehaviour
     {
         List<GameObject> scored_cards = new List<GameObject>();
         List<GameObject> first_pair_check = check_pair(played_cards);
-        if (first_pair_check != null && first_pair_check.Count > 0) // checks if there is a pair
+        if (first_pair_check != default && first_pair_check.Count > 0) // checks if there is a pair
         {
             List<GameObject> second_pair_check = check_pair(played_cards);
-            if (second_pair_check != null && second_pair_check.Count > 0) // checks if there is a second pair
+            if (second_pair_check != default && second_pair_check.Count > 0) // checks if there is a second pair
             {
                 scored_cards = first_pair_check.Concat(second_pair_check).ToList();
             }
         }
-        return scored_cards;
+        return default;
     }
 
     public static List<GameObject> check_pair(List<GameObject> played_cards)
     {
         List<GameObject> scored_cards = new List<GameObject>();
-        return scored_cards;
+        return default;
     }
 
     public static List<GameObject> check_three_of_a_kind(List<GameObject> played_cards)
     {
         List<GameObject> scored_cards = new List<GameObject>();
-        return scored_cards;
+        return default;
     }
 
     public static BaseScoreData get_high_card(List<GameObject> played_cards)
     {
         BaseScoreData base_score = new BaseScoreData();
         // adjust base_score for high card
-        return base_score;
+        return default;
 
     }
 
